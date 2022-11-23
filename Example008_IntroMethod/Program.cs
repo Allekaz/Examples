2234937fadd06b3fe81e95689f3c08abac4fca2a
Alexey Azizov <alekazi@yandex.ru>
2022-11-23 22:34:04 +0300

    modified:   Example005_ConditionIfElse/Program.cs new file:   Example008_IntroMethod/Example008_IntroMethod.csproj new file:   Example008_IntroMethod/Program.cs#

---
+++ Example008_IntroMethod/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 23;
+int b1 = 14;
+int c1 = 56;
+int a2 = 1157;
+int b2 = 24;
+int c2 = 65;
+int a3 = 72;
+int b3 = 5;
+int c3 = 787;
+
+int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+
+int max = Max(max1, max2, max3);
+
+Console.WriteLine(max);
